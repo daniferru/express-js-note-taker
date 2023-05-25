6442af99110cea37285803374d7db7d022d671a8
Daniella <danferrufino@icloud.com>
2023-05-25 13:45:35 -0400

    fixed heroku routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,35 +1,44 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const { readFile, writeFile } = require('fs/promises');
 
 router.get('/notes', async (req, res) => {
-    const dbJSON = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-    res.json(dbJSON);
+    try {
+    // const dbJSON = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const rawNotes = await readFile("db/db.json", "utf8");
+    const notesArray = [].concat(JSON.parse(rawNotes));
+    res.json(notesArray)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    // res.json(dbJSON);
 });
 
-router.post('/notes', (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+router.post('/notes', async (req, res) => {
+    // const dbJSON = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const rawNotes = await readFile("db/db.json", "utf8");
+    const notesArray = [].concat(JSON.parse(rawNotes));
     const newFeed = {
         title: req.body.title,
         text: req.body.text,
         id: uuidv4(),
     };
     console.log("New Note: ", newFeed);
-
-    dbJSON.push(newFeed);
-    fs.writeFileSync('db/db.json', JSON.stringify(dbJSON));
-    res.json(dbJSON);
+    const newNoteArray = [...notesArray, newFeed]
+    // dbJSON.push(newFeed);
+    writeFile('db/db.json', JSON.stringify(newNoteArray))
+    .then(() => res.json({ msg:"ok" }));
+    // res.json(dbJSON);
 });
 
 // deletes the delete request
-router.delete('/notes/:id', (req, res) => {
-    let data = fs.readFileSync('db/db.json', 'utf8');
-    const dataJSON = JSON.parse(data);
-    const newBtn = dataJSON.filter((note) => {
-        return note.id !== req.params.id;
-    });
-    fs.writeFileSync('db/db.json', JSON.stringify(newBtn));
-    res.json("Notes deleted.");
+router.delete('/notes/:id', async (req, res) => {
+    let data = await readFile('db/db.json', 'utf8');
+    // const dataJSON = JSON.parse(data);
+    const notesArray = [].concat(JSON.parse(data));
+    const filteredArray = notesArray.filter((note) => note.id !== req.params.id)
+    writeFile('db/db.json', JSON.stringify(filteredArray))
+    .then(() => res.json({ msg:"Notes deleted" }));
 });
 
 module.exports = router;
